182803da21d1e494a234dfe282aaf89c4a2a25cf
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-06-13 20:30:43 +0200

    restor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,9 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import CharacterDetails from "./components/CharacterDetails";
-
 import CharacterList from "./components/CharacterList";
-import App from "./App";
 
 const router = createBrowserRouter([
-  {
-    path: "/app",
-    element: <App />,
-  },
   {
     path: "/",
     element: <CharacterList />,
